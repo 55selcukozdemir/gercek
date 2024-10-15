7634fd8a084f8d26a69d778b336a02bf040934b0
Selcuk Ozdemir <selcuk.ozdemir@adayazilim.com>
2024-10-15 04:57:35 +0300

    Verilerin kullanımı için np array oluşturuldu

--- generate_image.py
+++ generate_image.py
@@ -3,15 +3,23 @@ import ast
 import numpy as np
 # from PIL import Image
 
-images = np.array()
-labels = np.array()
+images = np.empty((0, 26, 34))
+labels = np.empty((0, 1))
 with open('dataset_eye_on_off.csv', mode ='r')as file:
   csvFile = csv.reader(file)
   i = 0 
   for lines in csvFile:
         if(i != 0):
-            lines[1] = ast.literal_eval(lines[1])
-            lines[1] = np.reshape(lines[1], (26, 34)).astype(np.uint8)
+            image = ast.literal_eval(lines[1])
+            image = np.reshape(image, (26, 34)).astype(np.uint8)
+            images = x = np.append(images, [image], axis=0)
+
+            label = lines[0]
+            if label == 'open':
+              labels = np.append(labels, [1])
+            else:
+              labels = np.append(labels, [0])
+                
             # print(lines[1])
             # print(type(lines[1]))
             # image = Image.fromarray(lines[1])
@@ -22,5 +30,17 @@ with open('dataset_eye_on_off.csv', mode ='r')as file:
         # if(i == 2):
         #     print(type(lines))
         #     break
-  print(type(csvFile))
+  # print(type(csvFile))
+print(type(images))
+print(images.shape)
+
+print(images[0])
+
+print(type(labels))
+print(labels.shape)
+
+print(labels[0])
+
+print(labels[2873])
+  
         
